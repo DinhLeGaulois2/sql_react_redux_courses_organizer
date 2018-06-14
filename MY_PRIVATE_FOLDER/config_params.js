04b84131083686364f3f4152a9a6b7f1929ccd54
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-06-14 03:57:53 -0400

    Server: Setup

--- MY_PRIVATE_FOLDER/config_params.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    username: "root",
-    password: "Info((Jussieu[[Mias<<",   // <----------------- Your Password here
-    database: "courses_organizer",
-    host: "127.0.0.1",
-    dialect: "mysql",
-}
